40736ae48cd91dcff24b1998b29861c46f162ec9
oppong <michaeloppong123@gmail.com>
2021-12-18 18:39:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
-function App() {
+import Header from "./components/Header/Header";
+import Total from "./components/TotalPotComponent/Total";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contestants from "./pages/Contestants/Contestants";
+
+const App = (props) => {
+  const params = useParams();
+  const location = useLocation();
+  const nav = useNavigate();
+  const [url, setUrl] = useState(1);
+
+  // console.log("PARAMS", params);
+  useEffect(() => {
+    getUrl();
+  }, []);
+
+  const getUrl = async () => {
+    const { pathname } = location;
+    switch (pathname) {
+      case "/":
+        setUrl(1);
+        return;
+      case "/home":
+        setUrl(1);
+        return;
+
+      case "/contestants":
+        setUrl(2);
+        return;
+      case "/about":
+        setUrl(3);
+        return;
+      default:
+        nav("/home");
+        return;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{  }}>
+      <Header />
+      {url === 1 && <Home />}
+      {url === 2 && <Contestants />}
+      {url === 3 && <About />}
     </div>
   );
-}
+};
 
 export default App;
